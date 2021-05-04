9c13edf680c2b4a93910c2ba56a3c8b3a40bb97d
seungkwon <yevgnenll@gmail.com>
2021-05-04 14:30:22 +0900

    refactor: 반복 코드를 제거한다.
    다음 작성할 테스트 코드와 중복 되는 부분이 있어 제거한다.

--- src/test/java/me/yevgnenll/racing/RacingTrackTest.java
+++ src/test/java/me/yevgnenll/racing/RacingTrackTest.java
@@ -2,18 +2,25 @@ package me.yevgnenll.racing;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RacingTrackTest {
 
+	private CarFactory carFactory;
+	private RacingTrack racingTrack;
+
+	@BeforeEach
+	void init() {
+		carFactory = new CarFactory("kaki,dodo", 5);
+		racingTrack = new RacingTrack(carFactory);
+	}
+
 	@Test
 	@DisplayName(value = "실행 회수는 유저가 입력한 회수와 동일하다")
 	void count() {
-		CarFactory carFactory = new CarFactory("kaki,dodo", 5);
-		RacingTrack game = new RacingTrack(carFactory);
-
-		assertThat(game.getCount()).isEqualTo(5);
+		assertThat(racingTrack.getCount()).isEqualTo(5);
 	}
 
 }
